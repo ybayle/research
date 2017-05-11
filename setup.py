f37d1663379a93608b465658a13786b1319a1590
Yann Bayle <bayle.yann@live.fr>
2017-05-11 17:19:55 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'bayle',
   packages = ['bayle'],
-  version = '1.0.3',
+  version = '1.0.4',
   description = 'A Python package that provides reproducible code and data for my scientific research',
   author = 'Yann Bayle',
   author_email = 'bayle.yann@live.fr',
   url = 'https://github.com/ybayle/research',
-  download_url = 'https://github.com/ybayle/research/archive/v1.0.3.tar.gz',
+  download_url = 'https://github.com/ybayle/research/archive/v1.0.4.tar.gz',
   keywords = ['satin', 'MIR', 'dataset', 'research', 'reproducibility'],
   classifiers = [],
 )
